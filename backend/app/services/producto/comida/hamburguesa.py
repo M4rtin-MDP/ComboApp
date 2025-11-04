17b4dcc632e62017aea9bee949d78a44dff23095
M4rtin-MDP <mdmarquesdepaiva@estudiantes.unsam.edu.ar>
2025-11-04 18:14:13 -0300

    Agregadas las clases vacías de los ingredientes en el archivo de hamburguesa

--- backend/app/services/producto/comida/hamburguesa.py
+++ backend/app/services/producto/comida/hamburguesa.py
@@ -2,7 +2,7 @@ from producto_service import Producto, CategoriaProducto
 from abc import ABC
 
 # categoria.COMIDA
-class Hambuerguesa(Producto):
+class Hamburguesa(Producto):
     def __init__(self) -> None:
         super().__init__(CategoriaProducto.COMIDA)
         
@@ -16,12 +16,40 @@ class Hambuerguesa(Producto):
         
 
 
-class DecoradorHamburguesa(Hambuerguesa, ABC):
+class DecoradorHamburguesa(Hamburguesa, ABC):
     def __init__(self) -> None:
         super().__init__()
         
 
-class (DecoradorHamburguesa):
+class QuesoCheddar(DecoradorHamburguesa):
     pass
 
-    
+class Tomate(DecoradorHamburguesa):
+    pass
+
+class Lechuga(DecoradorHamburguesa):
+    pass
+
+class Cebolla(DecoradorHamburguesa):
+    pass
+
+class CebollaCaramelizada(DecoradorHamburguesa):
+    pass
+
+class Bacon(DecoradorHamburguesa):
+    pass
+
+class Huevos(DecoradorHamburguesa):
+    pass
+
+class Jamon(DecoradorHamburguesa):
+    pass
+
+class Mayonesa(DecoradorHamburguesa):
+    pass
+
+class Ketchup(DecoradorHamburguesa):
+    pass
+
+class Mostaza(DecoradorHamburguesa):
+    pass
